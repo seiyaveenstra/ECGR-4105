a2d43492b384169eb0ae2e1552251ca528776d32
seiyaveenstra <160799969+seiyaveenstra@users.noreply.github.com>
2024-02-23 23:34:09 -0500

    Add files via upload

--- assignment_1.py
+++ assignment_1.py
@@ -9,7 +9,7 @@ X0 = np.ones((m,1))
 X0 = pd.DataFrame(X0)
 ex.insert(0,"intercept",X0)
 print(ex)
-#first explanatory variable
+
 x1 = ex.loc[:,['intercept','X1']]
 y = ex.Y
 theta = np.zeros(2)
@@ -34,12 +34,24 @@ def gd(x,y,theta,alpha,iterations):
         costHis[i] = cost(x,y,theta)
     return theta, costHis
 
+#problem 1
+#part 1
 xarray = x1.loc[:,['intercept','X1']].values
 yarray = y.values
 theta,costHis = gd(xarray,yarray,theta,alpha,iterations)
 print('theta = ',theta)
 print('cost history = ',costHis)
+#x1 linear model
 
+plt.scatter(x1.X1,y,)
+plt.grid()
+plt.xlabel('X1')
+plt.ylabel('Y')
+plt.show()
+
+#first explanatory variable linear regression
+
+plt.figure()
 plt.scatter(x1.X1,y,label = 'Training data')
 plt.plot(x1.X1,x1.dot(theta),label = 'linear regression')
 plt.grid()
@@ -48,7 +60,18 @@ plt.ylabel('Y')
 plt.title('linear regression graph')
 plt.legend()
 plt.show()
-#second expalnatory variable
+
+#plot of cost vs iterations for X1
+
+plt.plot(range(1, iterations + 1), costHis,)
+plt.grid()
+plt.xlabel('Iterations')
+plt.ylabel('Cost')
+plt.title('Cost over iterations')
+plt.show()
+
+#part 2
+
 plt.figure()
 x2 = ex.loc[:,['intercept','X2']]
 xarray = x2.loc[:,['intercept','X2']].values
@@ -56,6 +79,17 @@ yarray = y.values
 theta,costHis = gd(xarray,yarray,theta,alpha,iterations)
 print('theta = ',theta)
 print('cost history = ',costHis)
+
+#x2 linear model
+
+plt.scatter(x2.X2,y,)
+plt.grid()
+plt.xlabel('X2')
+plt.ylabel('Y')
+plt.show()
+plt.figure()
+
+#second expalnatory variable linear regression
 
 plt.scatter(x2.X2,y,label = 'Training data')
 plt.plot(x2.X2,x2.dot(theta),label = 'linear regression')
@@ -66,7 +100,17 @@ plt.title('linear regression graph')
 plt.legend()
 plt.show()
 
-#third expalnatory variable
+#plot of cost vs iterations for X2
+
+plt.plot(range(1, iterations + 1), costHis,)
+plt.grid()
+plt.xlabel('Iterations')
+plt.ylabel('Cost')
+plt.title('Cost over iterations')
+plt.show()
+
+#part 3
+
 plt.figure()
 x3 = ex.loc[:,['intercept','X3']]
 xarray = x3.loc[:,['intercept','X3']].values
@@ -75,6 +119,17 @@ theta,costHis = gd(xarray,yarray,theta,alpha,iterations)
 print('theta = ',theta)
 print('cost history = ',costHis)
 
+#x3 linear model
+
+plt.scatter(x3.X3,y,)
+plt.grid()
+plt.xlabel('X3')
+plt.ylabel('Y')
+plt.show()
+plt.figure()
+
+#third expalnatory variable linear regression
+
 plt.scatter(x3.X3,y,label = 'Training data')
 plt.plot(x3.X3,x3.dot(theta),label = 'linear regression')
 plt.grid()
@@ -82,4 +137,61 @@ plt.xlabel('X3')
 plt.ylabel('Y')
 plt.title('linear regression graph')
 plt.legend()
+plt.show()
+
+#plot of cost vs iterations for X3
+
+plt.plot(range(1, iterations + 1), costHis,)
+plt.grid()
+plt.xlabel('Iterations')
+plt.ylabel('Cost')
+plt.title('Cost over iterations')
+plt.show()
+
+
+
+
+#problem 2
+theta = np.zeros(4)
+iterations = 1500
+alpha = 0.1
+plt.figure()
+x = ex.loc[:,['intercept','X1','X2','X3']]
+xarray = x.loc[:,['intercept','X1','X2','X3']].values
+yarray = y.values
+theta,costHis = gd(xarray,yarray,theta,alpha,iterations)
+print('theta = ',theta)
+print('cost history = ',costHis)
+
+#linear model
+
+for column in x.columns[1:]:
+    plt.scatter(x[column], y, label=column)
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Scatter Plot of All Columns against Y')
+plt.legend()
+plt.grid(True)
+plt.show()
+plt.figure()
+
+#linear regression
+for column in x.columns[1:]:
+    plt.scatter(x[column], y, label=column)
+plt.plot(x,x.dot(theta),label ='linear regression')
+plt.grid()
+plt.xlabel('X3')
+plt.ylabel('Y')
+plt.title('linear regression graph')
+plt.legend()
+plt.show()
+plt.figure()
+
+# cost
+plt.plot(range(1, iterations + 1), costHis,)
+plt.grid()
+plt.xlabel('Iterations')
+plt.ylabel('Cost')
+plt.title('Cost over iterations')
 plt.show()
